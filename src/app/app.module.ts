22629c62934567863305c529e17cc6342c63d24f
Kuba Grodzki <kubagrodzki12@gmail.com>
2022-06-11 18:21:02 +0200

    dużo zmiany

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -26,7 +26,7 @@ import { ListaItemComponent } from './lista-item/lista-item.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { last } from 'rxjs';
+import { FormGroup,FormControl,Validators, AbstractControl} from '@angular/forms'
 import { EventsService } from '../events.service';
 import { GameEvent } from '../GameEvent';
 import { Player } from '../Player';
@@ -10,13 +10,15 @@ import { Player } from '../Player';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  playerForm=new FormGroup({
+    name:new FormControl('',[Validators.required]),
+    nickName:new FormControl('',[Validators.required]),
+    lastName:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]),
+    mobile:new FormControl('',[Validators.required,Validators.pattern('^[0-9]{9}$')]),
+    event:new FormControl('',[Validators.required])
+  });
   events!:GameEvent[];
-  nameInput!:string;
-  nickNameInput!:string;
-  lastNameInput!:string;
-  emailInput!:string;
-  mobileInput!:number;
-  eventInput!:number;
   constructor(private eventsService:EventsService) { }
   ngOnInit(): void {
     this.eventsService.getEvents().subscribe((events)=>this.events=events);
@@ -25,12 +27,16 @@ export class FormComponent implements OnInit {
   {
     const newPlayer:Player=
     {
-      name:this.nameInput,
-      lastName:this.lastNameInput,
-      email:this.emailInput,
-      mobile:this.mobileInput,
-      nickName:this.nickNameInput
+      name:this.playerForm.value.name,
+      nickName:this.playerForm.value.nickName,
+      lastName:this.playerForm.value.lastName,
+      email:this.playerForm.value.email,
+      mobile:this.playerForm.value.mobile
     }
-    this.eventsService.addPlayer(newPlayer,this.eventInput);
+    this.eventsService.addPlayer(newPlayer,this.playerForm.value.event);
+  }
+  get playerFormControls()
+  {
+    return this.playerForm.controls;
   }
 }
